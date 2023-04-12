5ef132602699c7a0c4ef6f3944f4f8bdcf31a7c9
Lucas Otoni <lotoni.vivo@gmail.com>
2023-04-12 15:22:13 -0300

    feat: structuring data scrapper for buscape

--- backend/src/utils/data_scraper.ts
+++ backend/src/utils/data_scraper.ts
@@ -5,42 +5,70 @@ async function meliScrapper(category: string) {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto(`https://lista.mercadolivre.com.br/${category}`);
-  page.evaluate(() => {
-    const cleiton = Array.from(
-      document.querySelectorAll(
-        '.ui-search-layout__item div.ui-search-result__image.shops__picturesStyles > a'
-      )
-    ) as HTMLAnchorElement[];
-    console.log(cleiton[0].href);
-    // const pi = document.querySelectorAll('.ui-search-layout__item');
-    
-  });
-  // page.evaluate(() => console.log(Array.from(document.querySelectorAll('.ui-search-layout__item .ui-search-link'))))
-  // page.evaluate(() => console.log(Array.from(document.querySelectorAll('.ui-search-layout__item')).map((product) => ({
-  //   title: product.querySelector('.ui-search-item__title')?.textContent,
-  //   price: product.querySelector('.price-tag-fraction')?.textContent,
-  //   link: product.querySelector('.ui-search-link')?.href,
-  // }))))
-  // await browser.close();
+  try {
+    const productsInfo = await page.evaluate((category) => {
+      const productsUrls = Array.from(
+        document.querySelectorAll(
+          '.ui-search-layout__item div.ui-search-result__image.shops__picturesStyles > a'
+        )
+      ) as HTMLAnchorElement[];
+      return Array.from(
+        document.querySelectorAll('.ui-search-layout__item')
+      ).map((product, i) => ({
+        photo: product.querySelector('img')?.src || '',
+        description:
+          product.querySelector('.ui-search-item__title')?.textContent || '',
+        price:
+          product.querySelector(
+            '.ui-search-price__second-line .price-tag-fraction'
+          )?.textContent || '',
+        link: productsUrls[i].href || '',
+        category,
+      }));
+    }, category);
+    await browser.close();
+    return productsInfo;
+  } catch (e) {
+    throw e;
+  }
 }
 
-async function buscaScrapper(category: string) {
-  const browser = await puppeteer.launch();
+async function buscapeScrapper(category: string) {
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto(`lista.mercadolivre.com/${category}`);
-  const pageTitle = await page.title();
-  await browser.close();
+  await page.goto(`https://www.buscape.com.br/${category}`);
+  try {
+    const productsInfo = await page.evaluate((category) => {
+      const productsUrls = Array.from(
+        document.querySelectorAll('.Card_Card__LsorJ > a')
+      ) as HTMLAnchorElement[];
+      return Array.from(document.querySelectorAll('.Card_Card__LsorJ')).map(
+        (product, i) => ({
+          photo: product.querySelector('img')?.getAttribute('src') || '',
+          description: product.querySelector('h2')?.textContent || '',
+          price:
+            product.querySelector(
+              '[role="button"] .Text_Text__h_AF6.Text_MobileHeadingS__Zxam2'
+            )?.textContent || '',
+          link: productsUrls[i].href || '',
+          category,
+        })
+      );
+    }, category);
+    await browser.close();
+    return productsInfo;
+  } catch (e) {
+    throw e;
+  }
 }
 
-export async function main({ url, category }: IScrapper) {
+export async function infoScrapper({ url, category }: IScrapper) {
   switch (url) {
     case 'mercadolivre':
       meliScrapper(category);
       break;
     case 'buscape':
-      buscaScrapper(category);
+      buscapeScrapper(category);
       break;
   }
 }
-
-main({ url: 'mercadolivre', category: 'celular' });
