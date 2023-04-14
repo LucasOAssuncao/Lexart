f30aa8cddbeb4a8a016c4614d979d83d893b6e14
Lucas Otoni <lotoni.vivo@gmail.com>
2023-04-14 16:27:04 -0300

    style: header css

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,22 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    screens: {
+      'sm': '220px',
+      // => @media (min-width: 320px)
+
+      'md': '620px',
+      // => @media (min-width: 820px)
+
+      'lg': '1366px',
+      // => @media (min-width: 1366px)
+
+      'xl': '1920px',
+      // => @media (min-width: 1920px)
+
+      'xxl': '2400px',
+      // => @media (min-width: 2560px)
+    },
   },
   plugins: [],
 };
