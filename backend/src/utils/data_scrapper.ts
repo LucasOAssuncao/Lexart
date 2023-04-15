91ecd7c174eaa58f4eb6bf3c4db94f35413c8413
Lucas Otoni <lotoni.vivo@gmail.com>
2023-04-15 17:17:40 -0300

    fix:  remove puppeteer add axios

--- backend/src/utils/data_scrapper.ts
+++ backend/src/utils/data_scrapper.ts
@@ -1,74 +1,45 @@
-import * as puppeteer from 'puppeteer';
+import axios from 'axios';
+import { JSDOM } from 'jsdom';
 import IProduct from '../interfaces/IProduct';
 import IScrapper from '../interfaces/IScrapper';
 
 async function meliScrapper(category: string): Promise<IProduct[]> {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(`https://lista.mercadolivre.com.br/${category}`);
-  try {
-    const productsInfo = await page.evaluate((category) => {
-      const productsUrls = Array.from(
-        document.querySelectorAll(
-          '.ui-search-layout__item div.ui-search-result__image.shops__picturesStyles > a'
-        )
-      ) as HTMLAnchorElement[];
-      return Array.from(
-        document.querySelectorAll('.ui-search-layout__item')
-      ).map((product, i) => ({
-        photo:
-          product.querySelector('img[data-src]')?.getAttribute('data-src') ||
-          product.querySelector('img')?.getAttribute('src') ||
-          '',
-        description:
-          product.querySelector('.ui-search-item__title')?.textContent || '',
-        price:
-          product.querySelector(
-            '.ui-search-price__second-line .price-tag-fraction'
-          )?.textContent || '',
-        link: productsUrls[i].href || '',
-        category,
-      }));
-    }, category);
-    await browser.close();
-    return productsInfo as IProduct[];
-  } catch (e) {
-    throw e;
-  }
+  const response = await axios.get(`https://lista.mercadolivre.com.br/${category}`);
+  const dom = new JSDOM(response.data);
+  const productsUrls = Array.from(
+    dom.window.document.querySelectorAll('.ui-search-layout__item div.ui-search-result__image.shops__picturesStyles > a')
+  ) as HTMLAnchorElement[];
+  return Array.from(dom.window.document.querySelectorAll('.ui-search-layout__item')).map((product, i) => ({
+    photo:
+      product.querySelector('img[data-src]')?.getAttribute('data-src') ||
+      product.querySelector('img')?.getAttribute('src') ||
+      '',
+    description:
+      product.querySelector('.ui-search-item__title')?.textContent || '',
+    price:
+      product.querySelector('.ui-search-price__second-line .price-tag-fraction')?.textContent || '',
+    link: productsUrls[i].href || '',
+    category,
+  })) as IProduct[];
 }
 
 async function buscapeScrapper(category: string): Promise<IProduct[]> {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(`https://www.buscape.com.br/${category}`);
-  try {
-    const productsInfo = await page.evaluate((category) => {
-      const productsUrls = Array.from(
-        document.querySelectorAll('.Card_Card__LsorJ > a')
-      ) as HTMLAnchorElement[];
-      return Array.from(document.querySelectorAll('.Card_Card__LsorJ')).map(
-        (product, i) => ({
-          photo:
-            product
-              .querySelector('noscript')
-              ?.textContent?.match(/src="([^"]*)"/)?.[1] ||
-            product.querySelector('img')?.getAttribute('src') ||
-            '',
-          description: product.querySelector('h2')?.textContent || '',
-          price:
-            product.querySelector(
-              '[role="button"] .Text_Text__h_AF6.Text_MobileHeadingS__Zxam2'
-            )?.textContent?.replace(/[^0-9\.-]+/,"") || '',
-          link: productsUrls[i].href || '',
-          category,
-        })
-      );
-    }, category);
-    await browser.close();
-    return productsInfo as IProduct[];
-  } catch (e) {
-    throw e;
-  }
+  const response = await axios.get(`https://www.buscape.com.br/${category}`);
+  const dom = new JSDOM(response.data, { runScripts: "dangerously" });
+  const productsUrls = Array.from(
+    dom.window.document.querySelectorAll('.Card_Card__LsorJ > a')
+  ) as HTMLAnchorElement[];
+  return Array.from(dom.window.document.querySelectorAll('.Card_Card__LsorJ')).map((product, i) => ({
+    photo:
+      product.querySelector('noscript')?.textContent?.match(/src="([^"]*)"/)?.[1] ||
+      product.querySelector('img')?.getAttribute('src') ||
+      '',
+    description: product.querySelector('h2')?.textContent || '',
+    price:
+      product.querySelector('[role="button"] .Text_Text__h_AF6.Text_MobileHeadingS__Zxam2')?.textContent?.replace(/[^0-9\.-]+/,'') || '',
+    link: productsUrls[i].href || '',
+    category,
+  })) as IProduct[];
 }
 
 async function mergeInfo(category: string): Promise<IProduct[]> {
